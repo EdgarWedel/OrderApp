bed006254b06521a9372a65387926817477696d0
EdgarWedel <edgar.wedel@web.de>
2026-03-19 14:04:52 +0100

    function basket full and empty

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function init() {
     let burgerRef = document.getElementById('burger_menu');
     let pizzaRef = document.getElementById('pizza_menu');
     let saladRef = document.getElementById('salad_menu');
+    let basketRef = document.getElementById('basket');
 
     for (let burgerIndex = 0; burgerIndex < foods.burgers.length; burgerIndex++) {
-        burgerRef.innerHTML += getBurgerTemplate(burgerIndex);   
+        burgerRef.innerHTML += getBurgerTemplate(burgerIndex);
     }
 
     for (let pizzaIndex = 0; pizzaIndex < foods.pizzas.length; pizzaIndex++) {
@@ -14,4 +15,38 @@ function init() {
     for (let saladIndex = 0; saladIndex < foods.salads.length; saladIndex++) {
         saladRef.innerHTML += getSaladTemplate(saladIndex);
     }
+
+    if (foods.basket.length == 0) {
+        basketRef.innerHTML = getEmptyBasketTemplate();
+    } else {
+        basketRef.innerHTML = "";
+        for (let basketIndex = 0; basketIndex < foods.basket.length; basketIndex++) {
+            basketRef.innerHTML += getBasketTemplate(basketIndex);
+        }
+    }
 };
+
+function addToBasketBurger(burgerIndex) {
+    let addedItem = {
+        "amount": 1,
+        "name": foods.burgers[burgerIndex].name,
+        "price": foods.burgers[burgerIndex].price
+    }
+
+    foods.basket.push(addedItem);
+    console.log(foods.basket);
+    renderBasket();
+}
+
+function renderBasket() {
+    let basket = document.getElementById('basket');
+    basket.innerHTML = "";
+
+    for (let basketIndex = 0; basketIndex < foods.basket.length; basketIndex++) {
+        basket.innerHTML += getBasketTemplate(basketIndex);
+    }
+}
+
+function calculateTotal() {
+
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -9,7 +9,7 @@ function getBurgerTemplate(burgerIndex) {
                     </div>
                     <div class="add-box">
                         <p class="price">${foods.burgers[burgerIndex].price}0 €</p>
-                        <button class="add-btn">Add to basket</button>
+                        <button onclick="addToBasketBurger(${burgerIndex})" id="add_btn" class="add-btn">Add to basket</button>
                     </div>
                 </div>
             </div>
@@ -28,7 +28,7 @@ function getPizzaTemplate(pizzaIndex) {
                     </div>
                     <div class="add-box">
                         <p class="price">${foods.pizzas[pizzaIndex].price}0 €</p>
-                        <button class="add-btn">Add to basket</button>
+                        <button onclick="addToBasket()" id="add_btn" class="add-btn">Add to basket</button>
                     </div>
                 </div>
             </div>
@@ -47,10 +47,46 @@ function getSaladTemplate(saladIndex) {
                     </div>
                     <div class="add-box">
                         <p class="price">${foods.salads[saladIndex].price}0 €</p>
-                        <button class="add-btn">Add to basket</button>
+                        <button onclick="addToBasket()"      id="add_btn" class="add-btn">Add to basket</button>
                     </div>
                 </div>
             </div>
         </section>
+    `
+}
+
+function getEmptyBasketTemplate() {
+    return `
+                <span class="empty-basket-info">
+                    Nothing here yet.
+                    Go ahead and choose something delicious!
+                </span>
+                <img class="empty-basket-icon" src="./assets/icons/shopping_cart.png" alt="">
+        `
+}
+
+function getBasketTemplate(basketIndex) {
+    return `
+            <div class="basket-food-box">
+                <div>
+                    <span class="amount">${foods.basket[basketIndex].amount} x</span>
+                    <span class="basket-name">${foods.basket[basketIndex].name}</span>
+                </div>    
+                <div class="nav-bar">
+                    <div class="nav-btns">
+                        <button class="delete-btn">
+                        <img class="icon default" src="./assets/icons/delete.png" alt="">
+                        <img class="icon hover" src="./assets/icons/deletehover.png" alt="">
+                        </button>
+                        <span class="amount">1</span>
+                        <button class="increase-btn"><img src="./assets/icons/+.png" alt=""></button>
+                    </div>
+                    <span class="basket-price">${foods.basket[basketIndex].price}0 €</span>
+                </div>
+            </div> 
+            <div>
+                <p>Subtotal</p><p>$
+
+            </div>   
     `
 }
